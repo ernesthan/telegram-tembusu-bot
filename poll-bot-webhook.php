19f720bbbfb44a85082320c5bdcbd1b116016506
Ernest Han <ernest.hec@gmail.com>
2016-02-19 13:14:48 +0800

    Fixed missing semi-colon

--- poll-bot-webhook.php
+++ poll-bot-webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'config.php'
+require_once 'config.php';
 require_once 'PollBot.php';
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -2,7 +2,7 @@
 
 set_time_limit(0);
 
-require_once 'config.php'
+require_once 'config.php';
 require_once 'PollBot.php';
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
